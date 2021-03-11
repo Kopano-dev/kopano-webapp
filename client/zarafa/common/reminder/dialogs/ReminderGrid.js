88164eb18b893ba1160823accdf3b46891da02c5
Snehal Dangroshiya <s.dangroshiya@kopano.com>
2021-03-11 14:01:52 +0530

    Remove context menu from reminder grid.
    - Remove reminder context menu from reminder grid and
    from other places too.
    
    Reference: KW-3694

--- client/zarafa/common/reminder/dialogs/ReminderGrid.js
+++ client/zarafa/common/reminder/dialogs/ReminderGrid.js
@@ -22,9 +22,15 @@ Zarafa.common.reminder.dialogs.ReminderGrid = Ext.extend(Zarafa.common.ui.grid.G
 		});
 
 		Zarafa.common.reminder.dialogs.ReminderGrid.superclass.constructor.call(this, config);
+	},
 
+	/**
+	 * Initialize the event handlers
+	 * @protected
+	 */
+	initEvents : function()
+	{
 		this.on('rowdblclick', this.onRowDblClick, this);
-		this.on('rowcontextmenu', this.onRowContextMenu, this);
 	},
 
 	/**
@@ -81,39 +87,6 @@ Zarafa.common.reminder.dialogs.ReminderGrid = Ext.extend(Zarafa.common.ui.grid.G
 		if (record) {
 			Zarafa.common.Actions.openReminderRecord(record);
 			this.dialog.close();
-		}
-	},
-
-	/**
-	 * Event handler which is triggered when the user right clicks on a row in
-	 * {@link Zarafa.common.reminder.dialogs.ReminderGrid ReminderGrid}.
-	 *
-	 * @param {Zarafa.common.reminder.dialogs.ReminderGrid} grid The grid which was right clicked
-	 * @param {Number} rowIndex The index number of the row which was right clicked
-	 * @param {Ext.EventObject} event The event structure
-	 * @private
-	 */
-	onRowContextMenu: function(grid, rowIndex, event)
-	{
-		var sm = this.getSelectionModel();
-
-		if (sm.hasSelection()) {
-			// Some records were selected...
-			if (!sm.isSelected(rowIndex)) {
-				// But none of them was the record on which the
-				// context menu was invoked. Reset selection.
-				sm.clearSelections();
-				sm.selectRow(rowIndex);
-			}
-		} else {
-			// No records were selected,
-			// select row on which context menu was invoked
-			sm.selectRow(rowIndex);
-		}
-
-		var records = sm.getSelections();
-		if (records) {
-			Zarafa.core.data.UIFactory.openContextMenu(Zarafa.core.data.SharedComponentType['common.contextmenu.reminder.remindergrid'], records, { position: event.getXY() });
 		}
 	}
 });
